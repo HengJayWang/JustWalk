fb7115551e330ac166cc0fea9edfc602df7b1f00
HengJay <hengjay.wang@itri.org.tw>
2022-03-03 15:01:11 +0800

    Initial Commit

---
+++ JustWalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JustWalk
+//
+//  Created by M100-M1MacMini on 2022/3/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JustWalk/JustWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWalkApp.swift
+//  JustWalk
+//
+//  Created by M100-M1MacMini on 2022/3/3.
+//
+
+import SwiftUI
+
+@main
+struct JustWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
